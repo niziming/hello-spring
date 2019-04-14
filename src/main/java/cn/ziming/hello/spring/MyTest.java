f8aa412218b639d7499f92802179e207df309b27
ziming <martiont@163.com>
2019-04-14 21:05:44 +0800

    Log4j和slf4j

--- src/main/java/cn/ziming/hello/spring/MyTest.java
+++ src/main/java/cn/ziming/hello/spring/MyTest.java
@@ -1,14 +1,32 @@
 package cn.ziming.hello.spring;
 
 import cn.ziming.hello.spring.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
+    // 记录日志的类
+    private static final Logger logger = LoggerFactory.getLogger(MyTest.class);
+
     public static void main(String[] args) {
         // IoC的实现依赖注入
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-context.xml");
         UserService userService = (UserService) applicationContext.getBean("userService");
         userService.sayHi();
+        // 信息
+        logger.info("info级别的日志");
+        // 调试级别的信息 例:查询出来的值
+        logger.debug("debug级别的日志");
+        // 警告级别的信息
+        logger.warn("warn级别的日志");
+        // 错误级别的信息
+        logger.error("error级别的日志");
+
+        String message = "测试";
+        String message1 = "测试1";
+
+        logger.info("message is : {} {}", message, message1);
     }
 }
--- src/test/java/cn/ziming/hello/spring/service/test/UserServiceTest.java
+++ src/test/java/cn/ziming/hello/spring/service/test/UserServiceTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import static org.junit.Assert.*;
+
 public class UserServiceTest {
 
     private UserService userService;
@@ -27,6 +29,36 @@ public class UserServiceTest {
         System.out.println("Hello JUnit");
     }
 
+    /**
+     * 测试断言
+     */
+    @Test
+    public void testAssert() {
+        String obj1 = "junit";
+        String obj2 = "junit";
+        String obj3 = "test";
+        String obj4 = "test";
+        String obj5 = null;
+        int var1 = 1;
+        int var2 = 2;
+        int[] arithmetic1 = {1, 2, 3};
+        int[] arithmetic2 = {1, 2, 3};
+        // Equals值比较
+        assertEquals(obj1, obj2);
+        // Same地址比较 地址引用
+        assertSame(obj3, obj4);
+
+        assertNotSame(obj2, obj4);
+
+        assertNotNull(obj1);
+
+        assertNull(obj5);
+        // user.get(i) 断言用户名为 admin 结果不是
+        // 就说明这里有问题
+        assertTrue("为真", var1 == var2);
+
+        assertArrayEquals(arithmetic1, arithmetic2);
+    }
 
     @After
     public void after() {
